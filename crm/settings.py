436d9f7686d53808000bc70e3d2bd0774a91de95
karamfm <www.karam777krm@gmail.com>
2021-09-06 15:36:44 +0300

    update secrit key

--- crm/settings.py
+++ crm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%6@t@2$l%p3klt@&+m1a#pmx#3ad=pupmjead9)zi2ep_n7ou5'
+SECRET_KEY = os.environ.get('SECRET_KEY_GIT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hhi192365@gmail.com'
-EMAIL_HOST_PASSWORD = '10mz10mz'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 
