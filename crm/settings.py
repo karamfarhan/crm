de4352120f77153592ef9698f1efd2bf20c2a61a
karamfarhan <www.karam777krm@gmail.com>
2022-04-24 19:01:37 +0300

    settings fix2

--- crm/settings.py
+++ crm/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =  # your Email
-EMAIL_HOST_PASSWORD =  # your Email Password
+EMAIL_HOST_USER = "" # your Email
+EMAIL_HOST_PASSWORD =  "" # your Email Password
 
 
 
